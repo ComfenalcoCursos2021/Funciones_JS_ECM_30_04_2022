9753bf6b5b6abb4502d00917840da08957e54d1b
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-05-03 19:16:38 -0500

    Crea una función mediante expresión.

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-
-
-
-console.log(saludar());
-
-function saludar(nombre=`Marcos`){
-    return `Hola usuario ${nombre} como estas :)`;
+const saludo = function (nombre="Marcos"){
+    return `Hola usuario ${nombre} como estas`;
 }
-
-
-// function saludar(sistema,altura,nombre="Marcos", edad=100){
-//     return `Hola usuario ${nombre} como estas le habla el sistema: ${sistema} tu altura es: ${altura}, tienes la misma edad mia: ${edad}`;
-// };
-// let lista = new Array(4).fill(null,0,2).fill(undefined,2);
-// Object.preventExtensions(lista);
-// let mensaje = saludar(...lista);
-// console.log(mensaje);
+console.log(saludo());
