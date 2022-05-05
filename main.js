270c457bd129b9e6a0dc247a4666d408ab6ecb55
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-05-05 18:42:53 -0500

    Solucion de la Actividad

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 // Crear un formulario con mínimo 10 campos de datos obtener 
 // la información y por medio de un botón ejecutar un función 
 // que muestre los datos capturados
+const mostrar = function(mensaje="No enviado",callback) {
+    document.querySelector(".mensaje").insertAdjacentHTML("beforeend", `<b>${mensaje} : <span>${callback.value}</span> </b> enviado desde el formulario<br>`);
+};
 
-// JS
-// function mostrar(){
-//     console.log("Di Clik");
-// }
-
-// EMC
-// document.querySelector("#btn").addEventListener("click", function(e){
-//     console.log(e);
-// })
+addEventListener("submit", function (e){
+    let input = e.target;
+    document.querySelector(".mensaje").innerHTML = null;
+    mostrar("Tipo de documento", input.dato1);
+    mostrar("Numero", input.dato2);
+    mostrar("Primer Nombre", input.dato3);
+    mostrar("Segundo Nombre", input.dato4);
+    mostrar("Primer Apellido", input.dato5);
+    mostrar("Segundo Apellido", input.dato6);
+    mostrar("Edad", input.dato7);
+    mostrar("Dirrecion", input.dato8);
+    mostrar("Telefono", input.dato9);
+    mostrar("Celular", input.dato10);
+    e.preventDefault();
+})
